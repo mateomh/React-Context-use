c7a80864fae73f54bf34371f9a263ad1562e484a
Mateo mojica <mmojicah.84@gmail.com>
2022-06-06 19:42:22 -0500

    Create home component boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <div>
-        <Route path='/'></Route>
-        <Route path='/cart'></Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+        <Route path='/cart'>
+
+        </Route>
       </div>
     </BrowserRouter>
   );
